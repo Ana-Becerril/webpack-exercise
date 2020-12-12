6e9651d78835d179941a7428dc806c2b083cc403
Ana- Becerril <ana.becbel@gmail.com>
2020-12-11 22:15:18 -0600

    used webpack to minimize css and load a image

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import _ from 'lodash';
+import './style.css';
+import Image from './suculentas.jpg';
 
 function component() {
     const element = document.createElement('div');
@@ -7,7 +9,14 @@ function component() {
   // Lodash, now imported by this script
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello');
+// Add the image to our existing div.
+const myImage = document.createElement("img");
+myImage.src = Image;
+
+element.appendChild(myImage);
+
+
     return element;
   }
   
